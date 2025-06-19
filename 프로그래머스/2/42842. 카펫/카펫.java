1ecd37b9a0ce4075ab54b9fa0be3a19cea9c80ba
서승 <seoseuo@naver.com>
2025-06-20 01:17:57 +0900

    Update 카펫.java

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,7 +4,7 @@ class Solution {
         int[] answer = new int[2];
         int extent = brown + yellow;
         
-        for(int h = 3; h<=extent/2; h++) {
+        for(int h = 3; h<=extent; h++) {
             if(extent%h!=0) continue;
             
             int w = extent/h;
